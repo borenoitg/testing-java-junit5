52571d5cf04a5171ef1d6072f47456d1c7103666
borenoitg <daniel.anani@outlook.com>
2022-02-24 16:58:01 +0100

    JUnit 5 : Grouped Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    @Test
+    void groupedAssertions() {
+        // given
+        Person person = new Person(1l,"Joe","Buck");
+        // Then
+        assertAll("Test Props Set",
+                ()->assertEquals(person.getFirstName(),"Joe"),
+                ()->assertEquals(person.getLastName(),"Buck"));
+    }
+
+    @Test
+    void groupedAssertionsMsgs() {
+        // given
+        Person person = new Person(1l,"Joe","Buck");
+        // Then
+        assertAll("Test Props Set",
+                ()->assertEquals(person.getFirstName(),"Joe", ()->"First Name Failed"),
+                ()->assertEquals(person.getLastName(),"Buck",  "Last Name Failed"));
+    }
+}
