22c115697af0e0a7326fff862ade757bf3afdc69
borenoitg <daniel.anani@outlook.com>
2022-02-25 16:39:26 +0100

    JUnit 5 : Tagging and Filtering JUnit Tests Après avoir positionner les tags, il faut ajouter dans l'ide (intelliJ) la configuration de test avec Tags et le nom du tag

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -7,12 +7,14 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
 import guru.springframework.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Tag("controllers")
 class VetControllerTest {
 
     SpecialtyService specialtyService;
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,27 +1,30 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Tag("model")
 class PersonTest {
     @Test
     void groupedAssertions() {
         // given
-        Person person = new Person(1l,"Joe","Buck");
+        Person person = new Person(1l, "Joe", "Buck");
         // Then
         assertAll("Test Props Set",
-                ()->assertEquals(person.getFirstName(),"Joe"),
-                ()->assertEquals(person.getLastName(),"Buck"));
+                () -> assertEquals(person.getFirstName(), "Joe"),
+                () -> assertEquals(person.getLastName(), "Buck"));
     }
 
     @Test
     void groupedAssertionsMsgs() {
         // given
-        Person person = new Person(1l,"Joe","Buck");
+        Person person = new Person(1l, "Joe", "Buck");
         // Then
         assertAll("Test Props Set",
-                ()->assertEquals(person.getFirstName(),"Joe", ()->"First Name Failed"),
-                ()->assertEquals(person.getLastName(),"Buck",  "Last Name Failed"));
+                () -> assertEquals(person.getFirstName(), "Joe", () -> "First Name Failed"),
+                () -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
     }
 }
