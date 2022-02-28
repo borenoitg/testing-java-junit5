e3c63e9eed8f1cccdf5d4a99125d2f38fe762019
borenoitg <daniel.anani@outlook.com>
2022-02-28 16:57:31 +0100

    JUnit 5 : Unit Tests vs Integration Tests Important : Il faut que le nom de la classe de test se termine par < IT >

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : eanani
+ * @project : testing-java-junit5
+ * @created : 28/02/2022,
+ **/
+class PetSDJpaServiceIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
