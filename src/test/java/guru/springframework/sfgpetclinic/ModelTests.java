52a1cc1424346f4e7e5f7196c5d7c3446b5c17e5
borenoitg <daniel.anani@outlook.com>
2022-02-28 10:35:40 +0100

    JUnit 5 : Test Interfaces

---
+++ src/test/java/guru/springframework/sfgpetclinic/ModelTests.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Tag;
+
+/**
+ * @author : eanani
+ * @project : testing-java-junit5
+ * @created : 28/02/2022
+ **/
+@Tag("model")
+public interface ModelTests {
+}
