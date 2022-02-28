2a6a3a2045558f7717e4a500d00c7e12c7ccb1f4
borenoitg <daniel.anani@outlook.com>
2022-02-28 17:04:39 +0100

    JUnit 5 : Extensions @ExtendWith

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import guru.springframework.sfgpetclinic.junitextensions.TimingExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * @author : eanani
  * @project : testing-java-junit5
  * @created : 28/02/2022,
  **/
+@ExtendWith(TimingExtension.class)
 class PetSDJpaServiceIT {
 
     @BeforeEach
