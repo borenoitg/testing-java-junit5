0c8c959f6f6e033b49ef1e2c64fb13b2b3414cb3
borenoitg <daniel.anani@outlook.com>
2022-02-28 15:28:42 +0100

    JUnit 5 : Parameterized Tests - ENUM Source

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/OwnerType.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author : eanani
+ * @project : testing-java-junit5
+ * @created : 28/02/2022,
+ **/
+public enum OwnerType {
+
+    INDIVIDUAL, COMPAGNY
+}
